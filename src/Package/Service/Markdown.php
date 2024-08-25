6cf137b20a5c40fa60167da5c7ba7deb2b6dc54c
remco van der Velde <remco.pc@outlook.com>
2024-08-25 21:11:50 +0200

    markdown from parsedown reimplemented, part 1

--- src/Package/Service/Markdown.php
+++ src/Package/Service/Markdown.php
@@ -5,8 +5,13 @@ use R3m\Io\App;
 
 use League\CommonMark\CommonMarkConverter;
 
+use League\CommonMark\Exception\CommonMarkException;
+
 class Markdown {
 
+    /**
+     * @throws CommonMarkException
+     */
     public static function parse(App $object, $flags, $options, $string=''): string
     {
         $converter = new CommonMarkConverter([
