0ab26f1ae88d77eefd346bd6dadaa045c55a45ed
remco van der Velde <remco.pc@outlook.com>
2024-08-25 21:15:58 +0200

    markdown from parsedown reimplemented, part 1

--- src/Package/Service/Markdown.php
+++ src/Package/Service/Markdown.php
@@ -12,8 +12,10 @@ class Markdown {
     /**
      * @throws CommonMarkException
      */
-    public static function parse(App $object, $flags, $options, $string=''): string
+    public static function parse(App $object, $string=''): string
     {
+        //options: App::options($object)
+        //flags: App::flags($object)
         $converter = new CommonMarkConverter([
             'html_input' => 'strip',
             'allow_unsafe_links' => false,
